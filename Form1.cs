b9c17f4edf177083f126c246b01a3eabca80b89b
FranciscodeJ <147544324+FranciscodeJ@users.noreply.github.com>
2023-10-20 15:18:51 -0500

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -29,14 +29,14 @@ namespace Práctica__4
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int a, b, c, d;
-            a = int.Parse(textBox1.Text);
+            int a, b, c, d;                                 // se declararon las variables a b c d
+            a = int.Parse(textBox1.Text);                   // se declaró quea cada varible se le asigne el valor que se le escriba a cada una
             b = int.Parse(textBox2.Text);
             c = int.Parse(textBox3.Text);
             d = int.Parse(textBox4.Text);
 
-            textBox5.Text = d.ToString();
-            textBox6.Text = c.ToString();
+            textBox5.Text = d.ToString();         // se declaró que en cada caja de texto de la derecha se muestre cada varible en orden inverso (de abajo hacia arriba).
+            textBox6.Text = c.ToString();         
             textBox7.Text = b.ToString();
             textBox8.Text = a.ToString();
 
