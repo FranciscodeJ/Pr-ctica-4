8a67d99a362d58cd6fe2a209427e7569b28c33b0
franc <franc@MAKINADEPANCHO>
2023-10-20 12:46:36 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace Práctica__4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Showcard Gothic", 16.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label1.Location = new Point(40, 51);
+            label1.Name = "label1";
+            label1.Size = new Size(163, 35);
+            label1.TabIndex = 0;
+            label1.Text = "Número A";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Showcard Gothic", 16.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label2.Location = new Point(40, 113);
+            label2.Name = "label2";
+            label2.Size = new Size(165, 35);
+            label2.TabIndex = 1;
+            label2.Text = "Número B";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Showcard Gothic", 16.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label3.Location = new Point(40, 172);
+            label3.Name = "label3";
+            label3.Size = new Size(163, 35);
+            label3.TabIndex = 2;
+            label3.Text = "Número C";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Showcard Gothic", 16.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label4.Location = new Point(40, 232);
+            label4.Name = "label4";
+            label4.Size = new Size(167, 35);
+            label4.TabIndex = 3;
+            label4.Text = "Número D";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Showcard Gothic", 16.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label5.Location = new Point(406, 51);
+            label5.Name = "label5";
+            label5.Size = new Size(163, 35);
+            label5.TabIndex = 4;
+            label5.Text = "Número A";
+            label5.Click += label5_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Showcard Gothic", 16.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label6.Location = new Point(406, 113);
+            label6.Name = "label6";
+            label6.Size = new Size(165, 35);
+            label6.TabIndex = 5;
+            label6.Text = "Número B";
+            label6.Click += label6_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Showcard Gothic", 16.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label7.Location = new Point(406, 172);
+            label7.Name = "label7";
+            label7.Size = new Size(163, 35);
+            label7.TabIndex = 6;
+            label7.Text = "Número C";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Showcard Gothic", 16.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label8.Location = new Point(406, 232);
+            label8.Name = "label8";
+            label8.Size = new Size(167, 35);
+            label8.TabIndex = 7;
+            label8.Text = "Número D";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(241, 51);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 43);
+            textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(241, 113);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 43);
+            textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.Location = new Point(241, 167);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 43);
+            textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox4.Location = new Point(241, 224);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 43);
+            textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox5.Location = new Point(587, 51);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(125, 43);
+            textBox5.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            textBox6.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox6.Location = new Point(587, 108);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(125, 43);
+            textBox6.TabIndex = 13;
+            // 
+            // textBox7
+            // 
+            textBox7.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox7.Location = new Point(587, 167);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(125, 43);
+            textBox7.TabIndex = 14;
+            // 
+            // textBox8
+            // 
+            textBox8.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox8.Location = new Point(587, 224);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(125, 43);
+            textBox8.TabIndex = 15;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("UD Digi Kyokasho NK-B", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(264, 314);
+            button1.Name = "button1";
+            button1.Size = new Size(247, 80);
+            button1.TabIndex = 16;
+            button1.Text = "Invertir Orden";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.epic_fantasy_desktop_xo3dkfr1dhevajax;
+            ClientSize = new Size(808, 460);
+            Controls.Add(button1);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private Button button1;
+    }
+}
